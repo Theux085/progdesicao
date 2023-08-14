df6a1e56b26123c104ef16a4ca713d6dda358e8e
ART0059292 <srmanigomes09@gmail.com>
2023-08-14 11:48:25 -0300

    mengao

---
+++ lista041/questao02.py
@@ -0,0 +1,10 @@
+'''
+2) Desenvolver um programa que permita ao aluno responder qual a capital do Brasil. O programa deverá exibir se
+a resposta está certa ou errada.
+'''
+
+resposta_aluno = input("Qual é a capital do Brasil? ")
+if resposta_aluno== "Brasília":
+    print("Resposta correta!")
+else:
+    print("Resposta incorreta. A capital do Brasil é Brasília.")
---
+++ lista041/questao08.py
@@ -0,0 +1,10 @@
+'''
+8) Desenvolver um programa que pergunte um número inteiro qualquer e verifique se ele está na faixa de 1 a 10.
+'''
+
+num = int(input("Informe um número inteiro: "))
+
+if 1 <= num <= 10:
+    print("O número está na faixa de 1 a 10.")
+else:
+    print("O número não está na faixa de 1 a 10.")
